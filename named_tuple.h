2eeefd0d3a0e24d8f680eb7feeb8fab3f2f16570
Erez Strauss <erez@erezstrauss.com>
2019-01-27 16:57:58 -0500

    C++ Named Tuple - initial version

---
+++ named_tuple.h
@@ -0,0 +1,140 @@
+//
+// Author: Erez Strauss <erez@erezstrauss.com>
+// Date: 1/27/19.
+//
+
+#pragma once
+
+#ifndef NAMED_TUPLE_H
+#define NAMED_TUPLE_H
+
+//  https://stackoverflow.com/questions/6245735/pretty-print-stdtuple, by AndyG and others
+
+
+#include <iostream>
+#include <tuple>
+
+namespace {
+
+template<class... T>
+static inline std::ostream &operator<<(std::ostream &os, const std::tuple<T...> &_tup);
+
+template<class TupType, size_t... I>
+static inline std::ostream &tuple_print(std::ostream &os, const TupType &_tup, std::index_sequence<I...>)
+{
+    os << "(";
+    (...,(os << (I == 0 ? "" : ", ") << (std::get<I>(_tup))));
+    os << ")";
+    return os;
+}
+
+template<class... T>
+static inline std::ostream &operator<<(std::ostream &os, const std::tuple<T...> &_tup)
+{
+    return tuple_print(os, _tup, std::make_index_sequence<sizeof...(T)>());
+}
+
+}
+
+// -----------------------------------------
+
+template<typename VT, typename NT>
+struct NamedValue {
+    static inline const char* _typename { NT::_name };
+    using type = VT;
+    using namedtype = typename NT::type;
+
+    VT _data;
+    operator VT&() { return _data; }
+
+    VT& operator=(const VT& value) { _data = value; return _data; }
+    VT& operator=(VT&& value) { _data = std::move(value); return _data; }
+};
+
+template<char ... C>
+struct NamedType {
+    static const inline char _name[sizeof...(C)+1] { C ..., '\0' };
+    using type = NamedType;
+
+    template<size_t N>
+    auto operator=(const char (& s)[N]) {
+        return NamedValue<std::string, NamedType> { s };
+    }
+
+    template<typename T>
+    auto operator=(const T& v) { return NamedValue<T, NamedType> { v } ; }
+
+};
+
+template<typename ... TS>
+class NamedTuple : public std::tuple<TS...> {
+public:
+    using type = NamedTuple<TS...>;
+    using simple_tuple_t = typename std::tuple<typename TS::type ...>;
+
+    NamedTuple(TS&& ... ts) noexcept : std::tuple<TS...>(std::forward<TS>(ts)...) {
+        //static_assert(count<T, Types...>() == 1, "T must appear exactly once in ...Types");
+    }
+
+
+    template <typename>
+    constexpr static int named_type_find(int)
+    {
+        return -5;
+    }
+
+    template <typename T, typename Head, typename... Tail>
+    constexpr static int named_type_find(int current_index = 0)
+    {
+        return std::is_same<T, typename Head::namedtype>::value /* && current_index <= sizeof...(Tail) */
+               ? current_index
+               : named_type_find<T, Tail...>(current_index + 1);
+    }
+
+    template <typename T>
+    constexpr static int get_index()
+    {
+        //static_assert(count<T, Types...>() == 1, "T must appear exactly once in ...Types");
+        return named_type_find<T, TS...>();
+    }
+
+    template <typename T>
+    auto& get()
+    {
+        //        return std::get<named_type_find<T, TS...>()>(*this);
+        return std::get< (NamedTuple<TS...>::get_index<T>()) > (*this);
+    }
+
+    template <typename T>
+    auto& operator[](T t) {
+        return get<decltype(t)>();
+    }
+
+};
+
+template<typename VT, typename NT>
+inline std::ostream& operator<<(std::ostream& os, const NamedValue<VT, NT>& nv) {
+    os << nv._typename << ": " << nv._data;
+    return os;
+}
+
+template<class CharT, CharT... cs>
+inline constexpr auto operator "" _ ()
+{
+    return NamedType< cs ... > {};
+}
+
+
+template<char ... C>
+std::ostream& operator << (std::ostream& os, const NamedType<C...>& nt) {
+    os << "Namedtype: " << nt._name ;
+    return os;
+}
+
+// Return a std::tuple with values
+template<typename ... Ts>
+inline auto simple_tuple(std::tuple<NamedValue<Ts...>>& t) {
+    return std::tuple<Ts...>  (std::get<0>(t));
+}
+
+#endif //NAMED_TUPLE_H
---
+++ named_tuple_example.cpp
@@ -0,0 +1,73 @@
+
+// Some of the code is based on:
+//  https://stackoverflow.com/questions/6245735/pretty-print-stdtuple, by AndyG and others
+// http://blogs.microsoft.co.il/sasha/2015/01/28/implementing-tuple-part-4/
+
+#include <named_tuple.h>
+#include <iostream>
+
+template<typename ... TS>
+class Data : public std::tuple<std::decay_t<TS>...> {
+public:
+    Data(TS&& ... ts) noexcept : std::tuple<std::decay_t<TS>...>(std::forward<TS>(ts)...) {}
+
+};
+
+void example_1()
+{
+    auto t1 = std::make_tuple(("fieldA"_ = 123), ("B"_ = 100.111), ("lastone"_ = 999));
+    std::cout << "example 1: " << t1 << '\n';
+}
+
+void example_2()
+{
+    auto t2 = NamedTuple {("fieldA"_ = 123), ("B"_ = 100.111), ("lastone"_ = 999) };
+    std::cout << "example 2: " << t2 << '\n';
+    t2["fieldA"_] = 321;
+    std::cout << "example 2, changed fieldA: " << t2 << '\n';
+}
+
+
+int main()
+{
+
+    example_1();
+    example_2();
+
+    Data<std::string,double> x ( "hello", 555 );
+
+
+    auto z1 = "abc"_;
+    std::cout << "deep tuple: " << std::make_tuple("Hello", 0.1, std::make_tuple(1,2,3,"four",5.5), 'Z') << std::endl;
+    std::cout << "Data: " << x << " z1: " << z1 << '\n';
+
+    auto t1 = std::make_tuple(("ax"_ = 5), ("by"_ = 3.4), ("str1"_=std::string("hello world")), ("str2"_="blabla2"));
+    std::cout << "named tuple: " << t1 << '\n';
+    auto deep1 = std::make_tuple(("a1"_=33), "deeptuple"_ = t1);
+    std::cout << "deep deep 1: " << deep1 << '\n';
+
+
+    auto tx = std::make_tuple(("r"_ = 5), ("i"_ = 1));
+    std::cout << "tx two int: " << tx << " sizeof(tx): " << sizeof(tx) << '\n';
+    std::cout << "tx : " << std::get<0>(tx) << " sizeof(tx): " << sizeof(tx) << '\n';
+    (int&)std::get<0>(tx) = 3;
+    std::cout << "tx two int: " << tx << " sizeof(tx): " << sizeof(tx) << '\n';
+
+    auto onevalue = std::make_tuple(("field1"_ = 2.1)) ;
+    std::cout << "one value named tuple: " << onevalue << '\n';
+    auto nt1 = NamedTuple { ("fieldX"_ = 123), ("fieldY"_ = "test me") };
+    //auto nt1 = NamedTuple { ("fieldX"_ = 123) };
+
+    //std::cout << simple_tuple(onevalue) << '\n';
+    std::cout << "named tuple: " << nt1 << '\n';
+    // std::cout << "nt1.find_field_index : " << nt1.named_type_find< NamedValue<int, decltype("fieldX"_)>, NamedValue<int, decltype("fieldX"_)> > () << '\n';
+    std::cout << "nt1.find_field_index : " << nt1.named_type_find< decltype("fieldX"_), NamedValue<int, decltype("fieldX"_)> > () << '\n';
+
+    std::cout << "n1.get_index<>(): " << nt1.get_index< decltype("fieldX"_) >() << '\n';
+
+    std::cout << (nt1.get<decltype("fieldX"_)>() ) << '\n';
+    std::cout << nt1["fieldX"_] << '\n';
+    std::cout << nt1["fieldY"_] << '\n';
+
+    return 0;
+}
