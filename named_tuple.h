64c4aa164888230425b854b8de310c94837432d7
Erez Strauss <erez@erezstrauss.com>
2019-02-02 16:20:12 -0500

    reformatting using:  clang-format -style="{BasedOnStyle: Google, IndentWidth: 4, SpaceAfterTemplateKeyword: false}" -i

--- named_tuple.h
+++ named_tuple.h
@@ -8,203 +8,210 @@
 #ifndef NAMED_TUPLE_H
 #define NAMED_TUPLE_H
 
-
 #include <iostream>
 #include <tuple>
 
 namespace {
 
 template<class... T>
-static inline std::ostream &operator<<(std::ostream &os, const std::tuple<T...> &_tup);
+static inline std::ostream& operator<<(std::ostream& os,
+                                       const std::tuple<T...>& _tup);
 
 template<class TupType, size_t... I>
-static inline std::ostream &tuple_print(std::ostream &os, const TupType &_tup, std::index_sequence<I...>)
-{
+static inline std::ostream& tuple_print(std::ostream& os, const TupType& _tup,
+                                        std::index_sequence<I...>) {
     os << "(";
-    (...,(os << (I == 0 ? "" : ", ") << (std::get<I>(_tup))));
+    (..., (os << (I == 0 ? "" : ", ") << (std::get<I>(_tup))));
     os << ")";
     return os;
 }
 
 template<class... T>
-static inline std::ostream &operator<<(std::ostream &os, const std::tuple<T...> &_tup)
-{
+static inline std::ostream& operator<<(std::ostream& os,
+                                       const std::tuple<T...>& _tup) {
     return tuple_print(os, _tup, std::make_index_sequence<sizeof...(T)>());
 }
 
-}
+}  // namespace
 
 // -----------------------------------------
 
-//namespace named_tuple {
+// namespace named_tuple {
 
-// named_value - value type VT, name type NT - holds the value type in a tuple while knowing the named type as type property
+// named_value - value type VT, name type NT - holds the value type in a tuple
+// while knowing the named type as type property
 
 template<typename VT, typename NT>
 struct named_value {
-    static inline const char* _typename { NT::_name };
+    static inline const char* _typename{NT::_name};
     using type = VT;
     using namedtype = typename NT::type;
 
-  named_value(const VT& v) : _data (v) {}
-  named_value(VT&& v) : _data (std::move(v)) {}
-  named_value() = default;
-  named_value(const named_value&) = default;
-  named_value(named_value&&) = default;
-  named_value& operator=(const named_value&) = default;
-  named_value& operator=(named_value&&) = default;
+    named_value(const VT& v) : _data(v) {}
+    named_value(VT&& v) : _data(std::move(v)) {}
+    named_value() = default;
+    named_value(const named_value&) = default;
+    named_value(named_value&&) = default;
+    named_value& operator=(const named_value&) = default;
+    named_value& operator=(named_value&&) = default;
 
-  template<typename ... Args> explicit named_value(Args ... args) : _data (std::forward<Args...>(args)...) {}
-  
+    template<typename... Args>
+    explicit named_value(Args... args)
+        : _data(std::forward<Args...>(args)...) {}
+
     operator VT&() { return _data; }
     VT& get() { return _data; }
     const VT& get() const { return _data; }
 
-    VT& operator=(const VT& value) { _data = value; return _data; }
-    VT& operator=(VT&& value) { _data = std::move(value); return _data; }
-private:
-  VT _data;
+    VT& operator=(const VT& value) {
+        _data = value;
+        return _data;
+    }
+    VT& operator=(VT&& value) {
+        _data = std::move(value);
+        return _data;
+    }
+
+   private:
+    VT _data;
 };
 
 // named type - creates a type for a given string, field a tuple member.
 
-template<char ... C>
+template<char... C>
 struct named_type {
-    static const inline char _name[sizeof...(C)+1] { C ..., '\0' };
+    static const inline char _name[sizeof...(C) + 1]{C..., '\0'};
     using type = named_type;
 
     template<size_t N>
-    auto operator=(const char (& s)[N]) {
-        return named_value<std::string, named_type> { s };
+    auto operator=(const char (&s)[N]) {
+        return named_value<std::string, named_type>{s};
     }
 
     template<typename T>
-    auto operator=(const T& v) const { return named_value<T, named_type> ( v ) ; }
+    auto operator=(const T& v) const {
+        return named_value<T, named_type>(v);
+    }
 
     template<typename T>
-    auto operator=(T& v) const { return named_value<T, named_type> ( std::move(v) ) ; }
-
+    auto operator=(T& v) const {
+        return named_value<T, named_type>(std::move(v));
+    }
 };
 
 // named tuple - hold named values, inherits from std::tuple
 //  TS the named value types
 
-template<typename ... TS>
+template<typename... TS>
 class named_tuple : public std::tuple<TS...> {
-public:
-
+   public:
     using type = named_tuple<TS...>;
-    using simple_tuple_t = typename std::tuple<typename TS::type ...>;
+    using simple_tuple_t = typename std::tuple<typename TS::type...>;
 
-    template <typename>
-    constexpr static int named_type_count(int)
-    {
+    template<typename>
+    constexpr static int named_type_count(int) {
         return 0;
     }
 
-    template <typename T, typename Head, typename... Tail>
-    constexpr static int named_type_count(int current_index = 0)
-    {
-      return (std::is_same<T, typename Head::namedtype>::value ? 1 : 0)
-	+ named_type_count<T, Tail...>(current_index + 1);
+    template<typename T, typename Head, typename... Tail>
+    constexpr static int named_type_count(int current_index = 0) {
+        return (std::is_same<T, typename Head::namedtype>::value ? 1 : 0) +
+               named_type_count<T, Tail...>(current_index + 1);
     }
-  
-    template <typename T>
+
+    template<typename T>
     constexpr static void verify_named_type_count() {
-        static_assert(named_type_count<T, TS...>() == 1, "named type field, appears more than once in named tuple");
+        static_assert(
+            named_type_count<T, TS...>() == 1,
+            "named type field, appears more than once in named tuple");
     }
 
     named_tuple() noexcept : std::tuple<TS...>() {
-      (...,verify_named_type_count<typename TS::namedtype>());
+        (..., verify_named_type_count<typename TS::namedtype>());
     }
 
-    named_tuple(TS&& ... ts) noexcept : std::tuple<TS...>(std::forward<TS>(ts)...) {
-      (...,verify_named_type_count<typename TS::namedtype>());
+    named_tuple(TS&&... ts) noexcept
+        : std::tuple<TS...>(std::forward<TS>(ts)...) {
+        (..., verify_named_type_count<typename TS::namedtype>());
     }
 
-    template<typename ... CT>
-    named_tuple(named_tuple<CT ...>& ct) noexcept {
-      (...,(get<typename CT::namedtype>()=ct[typename CT::namedtype{}]));
+    template<typename... CT>
+    named_tuple(named_tuple<CT...>& ct) noexcept {
+        (..., (get<typename CT::namedtype>() = ct[typename CT::namedtype{}]));
     }
 
-    template <typename>
-    constexpr static int named_type_find(int) noexcept
-    {
+    template<typename>
+    constexpr static int named_type_find(int) noexcept {
         return -1;
     }
 
-    template <typename T, typename Head, typename... Tail>
-    constexpr static int named_type_find(int current_index = 0) noexcept
-    {
+    template<typename T, typename Head, typename... Tail>
+    constexpr static int named_type_find(int current_index = 0) noexcept {
         return std::is_same<T, typename Head::namedtype>::value
-               ? current_index
-               : named_type_find<T, Tail...>(current_index + 1);
+                   ? current_index
+                   : named_type_find<T, Tail...>(current_index + 1);
     }
-  
-    template <typename T>
-    constexpr static int get_index() noexcept
-    {
-      static_assert(named_type_count<T, TS...>() == 1, "named type can appear only once in a named tuple");
+
+    template<typename T>
+    constexpr static int get_index() noexcept {
+        static_assert(named_type_count<T, TS...>() == 1,
+                      "named type can appear only once in a named tuple");
         return named_type_find<T, TS...>();
     }
 
-    template <typename T>
-    auto& get() noexcept
-    {
-        return std::get<(get_index<T>())> (*this);
+    template<typename T>
+    auto& get() noexcept {
+        return std::get<(get_index<T>())>(*this);
     }
 
-    template <typename T>
+    template<typename T>
     auto& operator[](T t) noexcept {
         return get<decltype(t)>();
     }
 
-  template<typename F>
-  auto& foreach(F&& f) noexcept {
-    (...,f(get<typename TS::namedtype>()));
-    return *this;
-  }
+    template<typename F>
+    auto& foreach (F&& f) noexcept {
+        (..., f(get<typename TS::namedtype>()));
+        return *this;
+    }
 
-  // Return the std::tuple with values without named_types information
+    // Return the std::tuple with values without named_types information
 
-  inline std::tuple<typename TS::type ... >& simple_tuple() {
-    return reinterpret_cast< std::tuple< typename TS::type ... >& >  (*this);
-  }
-
+    inline std::tuple<typename TS::type...>& simple_tuple() {
+        return reinterpret_cast<std::tuple<typename TS::type...>&>(*this);
+    }
 };
 
-template<typename ... TT, typename ... ST>
-auto& operator << (named_tuple<TT...>&trg, named_tuple<ST...>&src) {
-  (...,(trg[typename ST::namedtype{}] = src[typename ST::namedtype{}]));
-  return trg;
+template<typename... TT, typename... ST>
+auto& operator<<(named_tuple<TT...>& trg, named_tuple<ST...>& src) {
+    (..., (trg[typename ST::namedtype{}] = src[typename ST::namedtype{}]));
+    return trg;
 }
 
-template<typename ... TT, typename ... NV>
-auto& operator << (named_tuple<TT...>&trg, const NV& ... src) {
-  (..., (trg[typename NV::namedtype{}] = src.get()));
-  return trg;
+template<typename... TT, typename... NV>
+auto& operator<<(named_tuple<TT...>& trg, const NV&... src) {
+    (..., (trg[typename NV::namedtype{}] = src.get()));
+    return trg;
 }
 
 template<class CharT, CharT... CS>
-inline constexpr auto operator "" _ ()
-{
-    return named_type< CS ... > {};
+inline constexpr auto operator"" _() {
+    return named_type<CS...>{};
 }
 
 //} // namespace named
 
-
 template<typename VT, typename NT>
-inline std::ostream& operator<<(std::ostream& os, const named_value<VT, NT>& nv) {
-  os << nv._typename << ": " << nv.get();
+inline std::ostream& operator<<(std::ostream& os,
+                                const named_value<VT, NT>& nv) {
+    os << nv._typename << ": " << nv.get();
     return os;
 }
 
-template<char ... C>
-std::ostream& operator << (std::ostream& os, const named_type<C...>& nt) {
-    os << "Namedtype: " << nt._name ;
+template<char... C>
+std::ostream& operator<<(std::ostream& os, const named_type<C...>& nt) {
+    os << "Namedtype: " << nt._name;
     return os;
 }
 
-
-#endif //NAMED_TUPLE_H
+#endif  // NAMED_TUPLE_H
--- named_tuple_example.cpp
+++ named_tuple_example.cpp
@@ -6,165 +6,146 @@
 #include <named_tuple.h>
 #include <iostream>
 
-//using namespace named;  // To be implemented.
+// using namespace named;  // To be implemented.
 
 // Example 1 shows named tuple creation and printing
-void example_1()
-{
-    auto t1 = std::make_tuple(("fieldA"_ = 123), ("B"_ = 100.111), ("lastone"_ = 999));
+void example_1() {
+    auto t1 = std::make_tuple(("fieldA"_ = 123), ("B"_ = 100.111),
+                              ("lastone"_ = 999));
     std::cout << "example 1:\n  " << t1 << '\n';
 }
 
 // Example 2 shows named_tuple and field modification using operator[]
-void example_2()
-{
-    auto t2 = named_tuple {("fieldA"_ = 123), ("B"_ = 100.111), ("lastone"_ = 999) };
+void example_2() {
+    auto t2 =
+        named_tuple{("fieldA"_ = 123), ("B"_ = 100.111), ("lastone"_ = 999)};
     std::cout << "example 2:\n  " << t2 << '\n';
     t2["fieldA"_] = 321;
     std::cout << "  changed fieldA: " << t2 << '\n';
 
-    auto pfoo = [](auto x) { std::cout << "    name: '" << x._typename << "' value: " << x.get() << '\n'; };
-    t2.foreach( pfoo );
+    auto pfoo = [](auto x) {
+        std::cout << "    name: '" << x._typename << "' value: " << x.get()
+                  << '\n';
+    };
+    t2.foreach (pfoo);
 }
 
-
-template<typename ... TS>
+template<typename... TS>
 class Data : public std::tuple<std::decay_t<TS>...> {
-public:
-  Data(TS&& ... ts) noexcept
-    : std::tuple<std::decay_t<TS>...>(std::forward<TS>(ts)...) {}
+   public:
+    Data(TS&&... ts) noexcept
+        : std::tuple<std::decay_t<TS>...>(std::forward<TS>(ts)...) {}
 };
 
 // Example 3 - shows simple tuple printing.
-void example_3()
-{
-  Data<std::string,double> simple_tuple ( "hello", 555 );
+void example_3() {
+    Data<std::string, double> simple_tuple("hello", 555);
 
-  std::cout << "example 3:\n  Data: " << simple_tuple << '\n';
-
+    std::cout << "example 3:\n  Data: " << simple_tuple << '\n';
 }
 
 // Example 4 - shows Named types and Named values
-void example_4()
-{
+void example_4() {
     auto abc = "abc"_;
     std::cout << "example 4:\n  abc: " << abc << '\n';
-    auto nv = named_value<int, decltype("xyz"_)> { 42 };
+    auto nv = named_value<int, decltype("xyz"_)>{42};
     std::cout << "   " << nv << '\n';
 }
 
-// Example 5 - if two fields have the same name - static asset will avoid, try to change fieldB to fieldA
-void example_5()
-{
-  // auto t5 = named_tuple {("fieldA"_ = 123), ("B"_ = 100.111), ("fieldA"_ = 4.5) }; - fails at compile time, as it detect two fields with same named-type.
-  auto t5 = named_tuple {("fieldA"_ = 123), ("B"_ = 100.111), ("fieldB"_ = 4.5) };
-  std::cout << "example 5:\n  " << t5 << '\n';
+// Example 5 - if two fields have the same name - static asset will avoid, try
+// to change fieldB to fieldA
+void example_5() {
+    // auto t5 = named_tuple {("fieldA"_ = 123), ("B"_ = 100.111), ("fieldA"_
+    // = 4.5) }; - fails at compile time, as it detect two fields with same
+    // named-type.
+    auto t5 =
+        named_tuple{("fieldA"_ = 123), ("B"_ = 100.111), ("fieldB"_ = 4.5)};
+    std::cout << "example 5:\n  " << t5 << '\n';
 }
 
-
 template<>
-inline std::ostream& operator<< (std::ostream& os, const named_value<std::string, decltype("address"_)>& nv) {
-  os << nv._typename << ": '" << nv.get() << "'";
+inline std::ostream& operator<<(
+    std::ostream& os,
+    const named_value<std::string, decltype("address"_)>& nv) {
+    os << nv._typename << ": '" << nv.get() << "'";
     return os;
 }
 
-// Example 6 shows defining a named tuple type Person, and using overrided operator << for specific field name.
+// Example 6 shows defining a named tuple type Person, and using overrided
+// operator << for specific field name.
 
-void example_6()
-{
-  
-  using Person = named_tuple<
-    named_value<unsigned, decltype("id"_)>
-    , named_value<float, decltype("age"_)>
-    , named_value<std::string, decltype("name"_)>
-    , named_value<std::string, decltype("address"_)>
-    >;
-  Person per;
-  per["id"_] = 111;
-  per["age"_] = 16.5;
-  per["name"_] = "Bob";
-  per["address"_] = "101 Main St. Big City";
-  std::cout << "example 6:\n  " << per << '\n';
+void example_6() {
+    using Person = named_tuple<named_value<unsigned, decltype("id"_)>,
+                               named_value<float, decltype("age"_)>,
+                               named_value<std::string, decltype("name"_)>,
+                               named_value<std::string, decltype("address"_)> >;
+    Person per;
+    per["id"_] = 111;
+    per["age"_] = 16.5;
+    per["name"_] = "Bob";
+    per["address"_] = "101 Main St. Big City";
+    std::cout << "example 6:\n  " << per << '\n';
 
-  auto naked_tuple = per.simple_tuple();
-  std::cout << "  get<0>(naked_tuple)  " << std::get<0>(naked_tuple) << '\n';
-  std::cout << "  get<1>(naked_tuple)  " << std::get<1>(naked_tuple) << '\n';
-  std::cout << "  get<2>(naked_tuple)  " << std::get<2>(naked_tuple) << '\n';
-  std::cout << "  get<3>(naked_tuple)  " << std::get<3>(naked_tuple) << '\n';
-  static_assert ( sizeof(naked_tuple) == sizeof(per), "std::uple size != named_tuple size !");
+    auto naked_tuple = per.simple_tuple();
+    std::cout << "  get<0>(naked_tuple)  " << std::get<0>(naked_tuple) << '\n';
+    std::cout << "  get<1>(naked_tuple)  " << std::get<1>(naked_tuple) << '\n';
+    std::cout << "  get<2>(naked_tuple)  " << std::get<2>(naked_tuple) << '\n';
+    std::cout << "  get<3>(naked_tuple)  " << std::get<3>(naked_tuple) << '\n';
+    static_assert(sizeof(naked_tuple) == sizeof(per),
+                  "std::uple size != named_tuple size !");
 }
 
-void example_7()
-{
-  
-  using Person = named_tuple<
-    named_value<unsigned, decltype("id"_)>
-    , named_value<float, decltype("age"_)>
-    , named_value<std::string, decltype("name"_)>
-    , named_value<std::string, decltype("address"_)>
-    >;
-  Person per;
-  per["id"_] = 111;
-  per["age"_] = 16.5;
-  per["name"_] = "Bob";
-  per["address"_] = "101 Main St. Big City";
-  std::cout << "example 7:\n   " << per << '\n';
-  auto update = named_tuple{("id"_ = 333), ("age"_=99.9)};
-  std::cout << "   " << (per << update) << '\n';
-
+void example_7() {
+    using Person = named_tuple<named_value<unsigned, decltype("id"_)>,
+                               named_value<float, decltype("age"_)>,
+                               named_value<std::string, decltype("name"_)>,
+                               named_value<std::string, decltype("address"_)> >;
+    Person per;
+    per["id"_] = 111;
+    per["age"_] = 16.5;
+    per["name"_] = "Bob";
+    per["address"_] = "101 Main St. Big City";
+    std::cout << "example 7:\n   " << per << '\n';
+    auto update = named_tuple{("id"_ = 333), ("age"_ = 99.9)};
+    std::cout << "   " << (per << update) << '\n';
 }
 
-void example_8()
-{
-  
-  using Person = named_tuple<
-    named_value<unsigned, decltype("id"_)>
-    , named_value<float, decltype("age"_)>
-    , named_value<std::string, decltype("name"_)>
-    , named_value<std::string, decltype("address"_)>
-    >;
-  Person per;
-  per["id"_] = 111;
-  per["age"_] = 16.5;
-  per["name"_] = "Bob";
-  per["address"_] = "101 Main St. Big City";
-  std::cout << "example 8:\n   " << per << '\n';
-  std::cout << "   " << (per << ("age"_ = 3) << ("id"_=1001)) << '\n';
-
+void example_8() {
+    using Person = named_tuple<named_value<unsigned, decltype("id"_)>,
+                               named_value<float, decltype("age"_)>,
+                               named_value<std::string, decltype("name"_)>,
+                               named_value<std::string, decltype("address"_)> >;
+    Person per;
+    per["id"_] = 111;
+    per["age"_] = 16.5;
+    per["name"_] = "Bob";
+    per["address"_] = "101 Main St. Big City";
+    std::cout << "example 8:\n   " << per << '\n';
+    std::cout << "   " << (per << ("age"_ = 3) << ("id"_ = 1001)) << '\n';
 }
 
-void example_9()
-{
-
-  auto t1 = named_tuple{("addr"_=(void*)example_9),("size"_=999), ("fldx"_=.333)};
-  auto t2 = named_tuple{("size"_=888), ("addr"_=(void*)example_8)};
-  std::cout << "example 8:"
-	    << "\n   t1: " << t1
-	    << "\n   t2: " << t2
-    ;
-  t1 << t2;  // t1 = t2 , but << is better as in mean update with fields from t2
-  std::cout 
-	    << "\n   t1: " << t1
-	    << "\n   t2: " << t2
-	    << '\n';
-
+void example_9() {
+    auto t1 = named_tuple{("addr"_ = (void*)example_9), ("size"_ = 999),
+                          ("fldx"_ = .333)};
+    auto t2 = named_tuple{("size"_ = 888), ("addr"_ = (void*)example_8)};
+    std::cout << "example 8:"
+              << "\n   t1: " << t1 << "\n   t2: " << t2;
+    t1 << t2;  // t1 = t2 , but << is better as in mean update with fields from
+               // t2
+    std::cout << "\n   t1: " << t1 << "\n   t2: " << t2 << '\n';
 }
 
-void example_10()
-{
-  auto t1 = named_tuple{("a"_=10),("b"_="nameA")};
-  using Data = named_tuple<
-    named_value<std::string, decltype("b"_)>
-    , named_value<int, decltype("a"_)>
-    >;
+void example_10() {
+    auto t1 = named_tuple{("a"_ = 10), ("b"_ = "nameA")};
+    using Data = named_tuple<named_value<std::string, decltype("b"_)>,
+                             named_value<int, decltype("a"_)> >;
 
-  auto t2 = Data(t1);
-  std::cout << "example 10 constructor with compatible named_tuple:\n   " << t1 << "\n   " << t2 << '\n';
+    auto t2 = Data(t1);
+    std::cout << "example 10 constructor with compatible named_tuple:\n   "
+              << t1 << "\n   " << t2 << '\n';
 }
 
-int main()
-{
-
+int main() {
     example_1();
     example_2();
     example_3();
@@ -177,32 +158,40 @@ int main()
     example_10();
 
     auto z1 = "abc"_;
-    std::cout << "deep tuple: " << std::make_tuple("Hello", 0.1, std::make_tuple(1,2,3,"four",5.5), 'Z') << std::endl;
-  
-    auto t1 = std::make_tuple(("ax"_ = 5), ("by"_ = 3.4), ("str1"_=std::string("hello world")), ("str2"_="blabla2"));
-    std::cout << "named tuple: " << t1 << '\n';
-    auto deep1 = std::make_tuple(("a1"_=33), "deeptuple"_ = t1);
-    std::cout << "deep deep 1: " << deep1 << '\n';
+    std::cout << "deep tuple: "
+              << std::make_tuple("Hello", 0.1,
+                                 std::make_tuple(1, 2, 3, "four", 5.5), 'Z')
+              << std::endl;
 
+    auto t1 = std::make_tuple(("ax"_ = 5), ("by"_ = 3.4),
+                              ("str1"_ = std::string("hello world")),
+                              ("str2"_ = "blabla2"));
+    std::cout << "named tuple: " << t1 << '\n';
+    auto deep1 = std::make_tuple(("a1"_ = 33), "deeptuple"_ = t1);
+    std::cout << "deep deep 1: " << deep1 << '\n';
 
     auto tx = std::make_tuple(("r"_ = 5), ("i"_ = 1));
     std::cout << "tx two int: " << tx << " sizeof(tx): " << sizeof(tx) << '\n';
-    std::cout << "tx : " << std::get<0>(tx) << " sizeof(tx): " << sizeof(tx) << '\n';
+    std::cout << "tx : " << std::get<0>(tx) << " sizeof(tx): " << sizeof(tx)
+              << '\n';
     (int&)std::get<0>(tx) = 3;
     std::cout << "tx two int: " << tx << " sizeof(tx): " << sizeof(tx) << '\n';
 
-    auto onevalue = std::make_tuple(("field1"_ = 2.1)) ;
+    auto onevalue = std::make_tuple(("field1"_ = 2.1));
     std::cout << "one value named tuple: " << onevalue << '\n';
-    auto nt1 = named_tuple { ("fieldX"_ = 123), ("fieldY"_ = "test me") };
+    auto nt1 = named_tuple{("fieldX"_ = 123), ("fieldY"_ = "test me")};
 
     std::cout << "named tuple: " << nt1 << '\n';
 
-    std::cout << "n1.get_index<>(): " << nt1.get_index< decltype("fieldX"_) >() << '\n';
+    std::cout << "n1.get_index<>(): " << nt1.get_index<decltype("fieldX"_)>()
+              << '\n';
 
     std::cout << nt1["fieldX"_] << '\n';
     std::cout << nt1["fieldY"_] << '\n';
 
-    auto ttree = named_tuple { ("f1"_ = 123), ("subtup"_ = named_tuple { ("s1"_ = 345), ("s2"_=564) }), ("f3"_ ="test me") };
+    auto ttree = named_tuple{
+        ("f1"_ = 123), ("subtup"_ = named_tuple{("s1"_ = 345), ("s2"_ = 564)}),
+        ("f3"_ = "test me")};
     std::cout << "tree: " << ttree << '\n';
     return 0;
 }
